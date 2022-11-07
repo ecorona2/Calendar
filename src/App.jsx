4dde1eb84f72e78373f1756eae69b6b3c10b97b2
Eric Corona <ecorona2@jhu.edu>
2022-11-07 18:54:25 -0500

    Adds routing capabilities to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World!</h1>
-    </div>
+      <Routes>
+          <Route
+              path="/Calendar/"
+              element={
+                  <h1>Welcome to the Calendar!</h1>
+              }
+          />
+          <Route path="*" element={
+              <h1>Page was not found!</h1>
+          } />
+      </Routes>
   )
 }
 
